d3735632074d211746e9153dcb14162ff69cc454
Alice Herrera <a.aliceherrera.com>
2025-06-11 16:12:25 -0300

    adicionando token para orders

--- order/tests/tests_viewsets/tests_order_viewset.py
+++ order/tests/tests_viewsets/tests_order_viewset.py
@@ -2,6 +2,8 @@ import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token #addauthentication
+
 
 from django.urls import reverse
 
@@ -15,24 +17,32 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory() #adduser
+        token = Token.objects.create(user=self.user) #addauthentication
+        token.save() #addauthentication
+
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title='mouse', price=100, category=[self.category])
         self.order = OrderFactory(product=[self.product])
 
     def test_order(self):
+        token = Token.objects.get(user__username=self.user.username) #addauthentication
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key) #addauthentication
         response = self.client.get(
             reverse('order-list', kwargs={'version': 'v1'})
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        order_data = json.loads(response.content)[0]
-        self.assertEqual(order_data['product'][0]['title'], self.product.title)
-        self.assertEqual(order_data['product'][0]['price'], self.product.price)
-        self.assertEqual(order_data['product'][0]['active'], self.product.active)
-        self.assertEqual(order_data['product'][0]['category'][0]['title'], self.category.title)
+        order_data = json.loads(response.content)
+        self.assertEqual(order_data['results'][0]['product'][0]['title'], self.product.title)
+        self.assertEqual(order_data['results'][0]['product'][0]['price'], self.product.price)
+        self.assertEqual(order_data['results'][0]['product'][0]['active'], self.product.active)
+        self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)
 
     def test_create_order(self):
+        token = Token.objects.get(user__username=self.user.username) #addauthentication
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key) #addauthentication
         category = CategoryFactory()
         product = ProductFactory()
         product.category.set([category])
