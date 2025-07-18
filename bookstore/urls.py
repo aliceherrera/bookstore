f1ef002b3a4d5ef101ea5099d5a6ffc98f5c02ae
Alice Herrera <a.aliceherrera.com>
2025-07-18 18:00:05 -0300

    teste urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -19,6 +19,8 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
@@ -26,4 +28,5 @@ urlpatterns = [
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),   
     path('api-token-auth/', obtain_auth_token, name='api_token-auth'),
+    path('', lambda request: HttpResponse("Hello, Django on PythonAnywhere!")),
 ]
